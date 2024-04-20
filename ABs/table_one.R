b52b090fc1c3587f74c9c0235e0ccb067d49ea15
elenidom <43578457+elenidom@users.noreply.github.com>
2024-04-20 17:03:42 +0100

    Update table_one.R

--- ABs/table_one.R
+++ ABs/table_one.R
@@ -45,41 +45,6 @@ preparation_before_tableOne <- function(myPERSON){
   myPERSON$to_smoking <- ifelse(is.na(myPERSON$smoking), "not recorded", myPERSON$to_smoking)
   myPERSON$to_smoking <- as.factor(myPERSON$to_smoking)
   
-  # find the 5 most common comorbidities:
-  #   comorbidity <- c("risk_charlson14", "risk_charlson15", "risk_charlson16", "risk_charlson17", "risk_charlson18", 
-  #                   "risk_charlson19", "risk_charlson20", "risk_charlson21", "risk_charlson22", 
-  #                   "risk_charlson23", "risk_charlson24", "risk_charlson25", "risk_charlson26", 
-  #                   "risk_charlson27", "risk_charlson28", "risk_charlson29", "risk_charlson30", "risk_qcancer31", "risk_qcancer32", 
-  #                     "risk_qcancer33", "risk_qcancer34", "risk_qcancer35", "risk_qcancer36", 
-  #                     "risk_qcancer37", "risk_qcancer38", "risk_qcancer39", "risk_qcancer40", 
-  #                     "risk_qcancer41", "risk_qcancer42", "risk_qcancer43", "risk_qcancer44", 
-  #                     "risk_qcancer45", "risk_qcancer46", "risk_qcancer47", "risk_qcancer48", 
-  #                     "risk_qcancer49", "risk_qcancer50", "risk_qcancer51", "risk_qcancer52", 
-  #                     "risk_qcancer53", "risk_qcancer54")
-  #   
-  #     comorbidity_index <- 0
-  #     
-  #     for(i in seq_along(comorbidity)){
-  #       comorbidity_index[i] <- which(colnames(rescoh)==comorbidity[i])
-  #     }
-  #     
-  #     comorbidity_index
-  #     
-  #     sum_comorbid <- 0
-  #     for(i in seq_along(comorbidity)){
-  #       sum_comorbid[i] <- sum(rescoh[comorbidity_index[i]])
-  #     }
-  #     
-  #     cbind(comorbidity, sum_comorbid)
-  #     sort(sum_comorbid)
-  
-  # see which comorbidities are the most common:
-  # risk_charlson15 => CVD
-  # risk_charlson16 => COPD
-  # risk_charlson20 => diabetes
-  # risk_charlson27 => moderate/several renal
-  # risk_charlson28 => MI
-  
   # create a factor indicating these comorbidities
   myPERSON$CVD <- ifelse(myPERSON$risk_charlson15 == 1, "CVD", 0)
   myPERSON$COPD <- ifelse((myPERSON$risk_charlson16 == 1 | myPERSON$risk_qcancer38 == 1), "COPD", 0)
